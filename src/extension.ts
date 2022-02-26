c11111e4fb1ee35c0d60976ed71cb5e1f605e6ed
Robbie Ostermann <robertostermann5@gmail.com>
2022-02-25 22:23:33 -0600

    Add slight delay to extension activation to allow for hover provider to be activated first.

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,8 @@ import phpDriver from "./php/phpDriver";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function activate(context: vscode.ExtensionContext) {
-  markdownDriver.Register();
-  phpDriver.Register();
+  setTimeout(() => {
+    markdownDriver.Register();
+    phpDriver.Register();
+  }, 1000);
 }
